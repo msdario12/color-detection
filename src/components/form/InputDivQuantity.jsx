867c6ef8c9583e8084e76cb76bac6c01f29e8f96
msdario12 <mansilladario94@gmail.com>
2023-06-25 10:51:55 -0300

    Changed input for quantity of divs to a react component

--- src/components/form/InputDivQuantity.jsx
+++ src/components/form/InputDivQuantity.jsx
@@ -1,8 +1,12 @@
-export default function InputDivQuantity() {
+export default function InputDivQuantity(props) {
+	const { handleChange } = props;
 	return (
 		<div>
 			<label htmlFor='num-div'>Seleccione la cantidad de divisiones: </label>
-			<select name='divsQty' id='num-div'>
+			<select
+				onChange={(e) => handleChange(e.target.value)}
+				name='divsQty'
+				id='num-div'>
 				<option value='2'>2</option>
 				<option value='5'>5</option>
 				<option value='10'>10</option>
