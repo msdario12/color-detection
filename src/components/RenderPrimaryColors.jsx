11a41fadc5e4e752fd7a10ba2f71943c4c7fb796
msdario12 <mansilladario94@gmail.com>
2023-06-27 01:12:49 -0300

    Move logic of primary color to a new web worker, added loader

--- src/components/RenderPrimaryColors.jsx
+++ src/components/RenderPrimaryColors.jsx
@@ -1,59 +1,52 @@
-import { useEffect, useState } from 'react';
-import getPrimaryColor from '../utils/getPrimaryColor';
-import getPrimaryColorHSL from '../utils/getPrimaryColorHSL';
+import { useEffect, useState, useRef } from 'react';
 import IndividualPrimaryColor from './IndividualPrimaryColor';
 
-function formatAvgColorsList(avgColors) {
-	let arrayForPrimaryColors = [];
-	for (let i = 0; i < avgColors.length; i++) {
-		arrayForPrimaryColors.push({
-			RGB: avgColors[i].fillColor.RGBArray,
-			HSL: avgColors[i].fillColor.HSLArray,
-		});
-	}
-	return arrayForPrimaryColors;
-}
+function useWorkerPrimaryColors() {}
 
 export default function RenderPrimaryColors(props) {
 	const { avgColors, colorMode, colorTolerance, divsQty } = props;
 	const [colorList, setColorList] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const workerRef = useRef(
+		new Worker('../src/workers/workerPrimaryColors.js', { type: 'module' })
+	);
+
+	// Get the current state of worker, and save in a variable
+	const worker = workerRef.current;
 
 	useEffect(() => {
-		const arrayForPrimaryColors = formatAvgColorsList(avgColors);
-		setColorList([]);
-		if (colorMode === 'RGB') {
-			// let rawPrimaryColor = await getPrimaryColor(arrayForPrimaryColors);
-			// setColorList(rawPrimaryColor[0].base.RGB);
-			getPrimaryColor(arrayForPrimaryColors, colorTolerance).then((res) => {
-				{
-					setColorList(res);
-				}
-			});
-			return;
-		}
-		if (colorMode === 'HSL') {
-			getPrimaryColorHSL(arrayForPrimaryColors, colorTolerance).then((res) => {
-				setColorList(res);
-			});
-		}
-	}, [avgColors, colorTolerance]);
+		setIsLoading(true);
+		worker.postMessage({
+			msg: 'get-primary-colors',
+			avgColors,
+			colorMode,
+			colorTolerance,
+		});
+	}, [avgColors, colorMode, colorTolerance]);
 
-	console.log(colorList);
+	worker.onmessage = (e) => {
+		if (e.data.primaryColor) {
+			setColorList(e.data.primaryColor);
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<div className='my-10'>
 			<h2 className='text-4xl mb-5'>Colors</h2>
 			<div className='flex gap-5 flex-wrap justify-center'>
-				{colorList.length > 0 &&
-					colorList.map((color, idx) => (
-						<IndividualPrimaryColor
-							divsQty={divsQty}
-							key={'C' + idx}
-							colorMode={colorMode}
-							color={color}
-						/>
-						// eslint-disable-next-line no-mixed-spaces-and-tabs
-					))}
+				{colorList.length > 0 && !isLoading
+					? colorList.map((color, idx) => (
+							<IndividualPrimaryColor
+								divsQty={divsQty}
+								key={'C' + idx}
+								colorMode={colorMode}
+								color={color}
+							/>
+							// eslint-disable-next-line no-mixed-spaces-and-tabs
+					  ))
+					: 'Cargando datos de los colores primarios...'}
 			</div>
 		</div>
 	);
---
+++ src/workers/workerPrimaryColors.js
@@ -0,0 +1,36 @@
+import getPrimaryColor from '../utils/getPrimaryColor';
+import getPrimaryColorHSL from '../utils/getPrimaryColorHSL';
+
+function formatAvgColorsList(avgColors) {
+	let arrayForPrimaryColors = [];
+	for (let i = 0; i < avgColors.length; i++) {
+		arrayForPrimaryColors.push({
+			RGB: avgColors[i].fillColor.RGBArray,
+			HSL: avgColors[i].fillColor.HSLArray,
+		});
+	}
+	return arrayForPrimaryColors;
+}
+
+self.onmessage = (e) => {
+
+	if (e.data.msg === 'get-primary-colors') {
+		const { avgColors, colorMode, colorTolerance } = e.data;
+		const arrayForPrimaryColors = formatAvgColorsList(avgColors);
+		if (colorMode === 'RGB') {
+			// let rawPrimaryColor = await getPrimaryColor(arrayForPrimaryColors);
+			// setColorList(rawPrimaryColor[0].base.RGB);
+			getPrimaryColor(arrayForPrimaryColors, colorTolerance).then((res) =>
+				self.postMessage({ primaryColor: res })
+			);
+			return;
+		}
+
+		if (colorMode === 'HSL') {
+			getPrimaryColorHSL(arrayForPrimaryColors, colorTolerance).then((res) =>
+				self.postMessage({ primaryColor: res })
+			);
+			return;
+		}
+	}
+};
