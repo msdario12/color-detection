ae8236834a5537df22136f64e581146e5ebd9b58
msdario12 <mansilladario94@gmail.com>
2023-07-01 10:23:45 -0300

    Show primary colors in a list view, more compact

--- src/components/IndividualPrimaryColor.jsx
+++ src/components/IndividualPrimaryColor.jsx
@@ -2,27 +2,43 @@ import createStringColor from '../utils/createStringColor';
 import getColorTextBasedInBackground from '../utils/getColorTextBasedInBackground';
 
 export default function IndividualPrimaryColor(props) {
-	const { color, colorMode, divsQty, onEnter, onLeave } = props;
+	const {
+		color,
+		colorMode,
+		divsQty,
+		onEnter,
+		onLeave,
+		className,
+		style,
+		isColorSmall,
+	} = props;
 	const dimension = '175px';
 	const divStyle = {
 		height: dimension,
-		width: dimension,
 		backgroundColor: createStringColor(color, colorMode),
 		color: getColorTextBasedInBackground(color, colorMode),
 	};
 	const percentage = Math.round(
 		(Number(color.similarColors.length) / Number(divsQty ** 2)) * 100
 	);
+
 	return (
 		<div
+			style={style}
 			onMouseEnter={onEnter}
 			onMouseLeave={onLeave}
-			className='text-center p-2 border bg-white shadow-xl'>
+			className={className}>
 			<div
 				style={divStyle}
-				className='flex justify-center items-center flex-col font-semibold'>
-				<h3>{createStringColor(color, colorMode)}</h3>
-				<p>Participación {percentage}%</p>
+				className='flex flex-col items-center justify-center font-semibold'>
+				{!isColorSmall ? (
+					<div>
+						<h3>{createStringColor(color, colorMode)}</h3>
+						<p>Participación {percentage}%</p>
+					</div>
+				) : (
+					''
+				)}
 			</div>
 		</div>
 	);
--- src/components/RenderPrimaryColors.jsx
+++ src/components/RenderPrimaryColors.jsx
@@ -1,6 +1,6 @@
 import IndividualPrimaryColor from './IndividualPrimaryColor';
 import useWorkerPrimaryColors from '../hooks/useWorkerPrimaryColors';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useHoverAnimation from '../hooks/useHoverAnimation';
 
 export default function RenderPrimaryColors(props) {
@@ -17,7 +17,9 @@ export default function RenderPrimaryColors(props) {
 		colorMode,
 		colorTolerance
 	);
+	const [colorWidth, setColorWidth] = useState('');
 	const { onEnter, onLeave } = useHoverAnimation();
+	const divRef = useRef(null);
 	useEffect(() => {
 		setTimeColorPrimary({
 			start: timeColorPrimary.start,
@@ -26,19 +28,34 @@ export default function RenderPrimaryColors(props) {
 		setColorPrimaryList(colorList);
 	}, [timeColorPrimary, colorList]);
 
+	useEffect(() => {
+		if (divRef.current.clientWidth && colorList.length > 0) {
+			const clientWidth = divRef.current.clientWidth;
+			const colorNumber = colorList.length;
+			setColorWidth(Math.floor(clientWidth / colorNumber));
+			console.log(colorWidth);
+		}
+	}, [divRef, colorList]);
+
+	const isColorSmall = colorWidth < 150;
+
 	return (
-		<div className='my-10'>
-			<h2 className='text-4xl mb-5'>Colors</h2>
-			<div className='flex gap-5 flex-wrap justify-center'>
+		<div ref={divRef} className='my-10'>
+			<h2 className='mb-5 text-4xl'>Colors</h2>
+			<p>Se seleccionaron {colorList.length} colores</p>
+			<div className='flex flex-wrap justify-center'>
 				{colorList.length > 0 && !isLoading
 					? colorList.map((color, idx) => (
 							<IndividualPrimaryColor
+								style={{ width: colorWidth }}
+								className={`text-center`}
 								onEnter={onEnter}
 								onLeave={onLeave}
 								divsQty={divsQty}
 								key={'C' + idx}
 								colorMode={colorMode}
 								color={color}
+								isColorSmall={isColorSmall}
 							/>
 							// eslint-disable-next-line no-mixed-spaces-and-tabs
 					  ))
