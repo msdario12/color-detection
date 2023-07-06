ea392dd2e645e119fe280ab0e582cef5d31ae4ed
msdario12 <mansilladario94@gmail.com>
2023-07-06 17:59:26 -0300

    Install react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.jsx';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<RouterProvider router={router} />
+);
