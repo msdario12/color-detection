d0f997fcab899eb4233529c072d351888974dc3b
msdario12 <mansilladario94@gmail.com>
2023-07-12 17:00:13 -0300

    Added responsive design in home page, tabs functionality

--- src/components/ConfigSection.jsx
+++ src/components/ConfigSection.jsx
@@ -8,6 +8,9 @@ import InformationOfCalculations from './InformationOfCalculations';
 import createStringColor from '../utils/createStringColor';
 import SkeletonImg from './SkeletonImg';
 import { ImgConfig } from './ImgConfig';
+import { Tabs } from 'flowbite-react';
+import { faPercent } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ConfigSection() {
 	const [colorMode, setColorMode] = useState('RGB');
@@ -42,80 +45,104 @@ export default function ConfigSection() {
 		}
 	}, [colorPrimaryList]);
 
-	return (
-		<section className='config-section grid-rows-[minmax(200px, 1fr)] relative mx-auto grid  grid-cols-5 px-5 lg:container'>
-			<div className='sticky top-0 col-span-2 row-span-3 h-[90vh] overflow-y-auto'>
-				<ImgConfig
-					className='col-span-2 col-start-1 row-span-2 row-start-1 rounded-md border border-slate-800 px-3 py-5 md:px-8'
-					setImgUrl={setImgUrl}
-					setImgBitMap={setImgBitMap}
-					colorMode={colorMode}
-					divsQty={divsQty}
-					colorTolerance={colorTolerance}
-					isLoading={isLoading}
-					handleChangeImage={handleChangeImage}
-				/>
-				<FormConfig
-					className='color z-10 col-span-2 col-start-1 row-span-1 row-start-3 my-3 grid gap-4 rounded-md border border-slate-800 px-3 py-5 dark:bg-slate-900 dark:bg-opacity-80 dark:text-slate-400 dark:backdrop-blur-sm md:grid-cols-2 md:px-8'
-					setColorTolerance={setColorTolerance}
-					setDivsQty={setDivsQty}
-					colorMode={colorMode}
-					setColorMode={setColorMode}
-				/>
-				<InformationOfCalculations
-					className='col-span-2 col-start-1 row-span-1 row-start-4 mb-10 rounded-md border border-slate-800 px-3 py-5 md:px-8'
-					timeColorPrimary={timeColorPrimary}
-					imgSizes={imgSizes}
-					divsQty={divsQty}
-					time={time}
-				/>
-			</div>
+	const configSection = (
+		<div className='sticky top-0 col-span-2 row-span-3 h-[90vh] overflow-y-auto'>
+			<ImgConfig
+				className='col-span-2 col-start-1 row-span-2 row-start-1 rounded-md border border-slate-800 px-3 py-5 md:px-8'
+				setImgUrl={setImgUrl}
+				setImgBitMap={setImgBitMap}
+				colorMode={colorMode}
+				divsQty={divsQty}
+				colorTolerance={colorTolerance}
+				isLoading={isLoading}
+				handleChangeImage={handleChangeImage}
+			/>
+			<FormConfig
+				className='color z-10 col-span-2 col-start-1 row-span-1 row-start-3 my-3 grid gap-4 rounded-md border border-slate-800 px-3 py-5 dark:bg-slate-900 dark:bg-opacity-80 dark:text-slate-400 dark:backdrop-blur-sm md:grid-cols-2 md:px-8'
+				setColorTolerance={setColorTolerance}
+				setDivsQty={setDivsQty}
+				colorMode={colorMode}
+				setColorMode={setColorMode}
+			/>
+			<InformationOfCalculations
+				className='col-span-2 col-start-1 row-span-1 row-start-4 mb-10 rounded-md border border-slate-800 px-3 py-5 md:px-8'
+				timeColorPrimary={timeColorPrimary}
+				imgSizes={imgSizes}
+				divsQty={divsQty}
+				time={time}
+			/>
+		</div>
+	);
 
-			<div className='col-span-3 col-start-3 row-start-1 my-auto'>
+	const primaryColorsSection = (
+		<div className='col-span-3 col-start-3 row-start-1 my-auto'>
+			{avgColors.length > 0 ? (
+				<RenderPrimaryColors
+					divsQty={divsQty}
+					avgColors={avgColors}
+					colorMode={colorMode}
+					colorTolerance={colorTolerance}
+					setTimeColorPrimary={setTimeColorPrimary}
+					setColorPrimaryList={setColorPrimaryList}
+				/>
+			) : (
+				'Esperando datos color primario'
+			)}
+		</div>
+	);
+
+	const resultSection = (
+		<div className='col-span-3 col-start-3 row-span-3 row-start-2 p-2 md:columns-1'>
+			<div style={imgStyle} className='my-auto md:px-3'>
 				{avgColors.length > 0 ? (
-					<RenderPrimaryColors
-						divsQty={divsQty}
-						avgColors={avgColors}
-						colorMode={colorMode}
-						colorTolerance={colorTolerance}
-						setTimeColorPrimary={setTimeColorPrimary}
-						setColorPrimaryList={setColorPrimaryList}
+					<img
+						onLoad={handleLoadImg}
+						className='h-auto max-h-screen w-full'
+						src={imgUrl}
+						ref={imgRef}
+						alt='Img to get analize'
 					/>
 				) : (
-					'Esperando datos color primario'
+					<SkeletonImg />
+				)}
+				{!isLoading ? (
+					<RenderPixelColors
+						avgColors={avgColors}
+						colorMode={colorMode}
+						imgSizes={imgSizes}
+						isLoading={isLoading}
+					/>
+				) : (
+					<SkeletonImg
+						style={{
+							width: imgSizes.renderSize.w,
+							height: imgSizes.renderSize.h,
+						}}
+					/>
 				)}
 			</div>
+		</div>
+	);
 
-			<div className='col-span-3 col-start-3 row-span-3 row-start-2 p-2 md:columns-1'>
-				<div style={imgStyle} className='my-auto md:px-3'>
-					{avgColors.length > 0 ? (
-						<img
-							onLoad={handleLoadImg}
-							className='h-auto max-h-screen w-full'
-							src={imgUrl}
-							ref={imgRef}
-							alt='Img to get analize'
-						/>
-					) : (
-						<SkeletonImg />
-					)}
-					{!isLoading ? (
-						<RenderPixelColors
-							avgColors={avgColors}
-							colorMode={colorMode}
-							imgSizes={imgSizes}
-							isLoading={isLoading}
-						/>
-					) : (
-						<SkeletonImg
-							style={{
-								width: imgSizes.renderSize.w,
-								height: imgSizes.renderSize.h,
-							}}
-						/>
-					)}
-				</div>
-			</div>
-		</section>
+	return (
+		<>
+			<section className='sm:hidden'>
+				<Tabs.Group aria-label='Full width tabs' style='fullWidth'>
+					<Tabs.Item active title='Configuración'>
+						{configSection}
+					</Tabs.Item>
+					<Tabs.Item title='Resultado'>
+						{primaryColorsSection}
+						{resultSection}
+					</Tabs.Item>
+				</Tabs.Group>
+			</section>
+
+			<section className='config-section grid-rows-[minmax(200px, 1fr)] relative mx-auto hidden grid-cols-5  px-5 lg:container sm:grid'>
+				{configSection}
+				{primaryColorsSection}
+				{resultSection}
+			</section>
+		</>
 	);
 }
