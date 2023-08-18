174ba8f0baac2c119b5ded2e70dc43af9490b456
msdario12 <mansilladario94@gmail.com>
2023-08-18 19:13:59 -0300

    build creation

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	root: 'src',
-	publicDir: 'public',
-	build: {
-		outDir: '../dist',
-	},
-	server: {
-		open: true,
-	},
-});
+  plugins: [react()],
+})
+;
